4b49a7672ba5c0eb107d783787d6ce1b8ce72d1c
chalindu888 <chalindu888@gmail.com>
2026-07-04 15:05:33 +0530

    change image width and add list

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import {createRoot} from 'react-dom/client'
 const root = createRoot(document.getElementById("root"))
 root.render(
-  <img src="reactlogo.png" alt="React Logo" />
+ <main>
+  <img src="reactlogo.png" width="40px" height="40px" alt="React Logo" />
+  <h1>This is another element</h1>
+  <ul>
+    <li>chalindu</li>
+    <li>Kalhara</li>
+    <li>Udumulla</li>
+    <li>kosgoda</li>
+    <li>campus</li>
+  </ul>
+ </main>  
 )
